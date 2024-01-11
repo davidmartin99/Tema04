1e85b6bf6d862e59e3a1de1e61858b67ebbe9cdc
David <davidmartinpulgar9@gmail.com>
2024-01-11 18:29:37 +0100

    new

--- src/individuo/Individuo.java
+++ src/individuo/Individuo.java
@@ -2,12 +2,17 @@ package individuo;
 
 /**
  * Calcular la poblacion, cada vez que se añade un individuo 
+ * añadir altura y calcular edad con el año actual
+ * tambien añade 1cm a la altura
  * @author David
  */
 public class Individuo {
     private String dni, nombre;
     private int fechaNacimiento;
     private static int poblacion; //static para que pueda ir cambiando 
+    private static int añoActual = 2024;
+    private int edad;
+    private double altura;
     
     public Individuo(){} //Fin Metodo Constructor 1
     
@@ -16,6 +21,17 @@ public class Individuo {
         this.dni = dni;
         this.nombre = nombre;
         this.fechaNacimiento = fechaNacimiento;
+        poblacion++; //Se asume que partira de valor 0
+    }//Fin Metodo Constructor 2
+    
+    //Creamos otro metodo constructor igual añadiendo altura
+    public Individuo(String dni, String nombre, int fechaNacimiento, double altura) {
+        super(); //Primera linea del metodo constructor que invoque a la clase superior que comparta el mismo tipo de parametrizacion
+        this.dni = dni;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.altura = altura;
+        edad = añoActual - fechaNacimiento;
         poblacion++; //Se asume que partira de valor 0
     }//Fin Metodo Constructor 2
     
@@ -29,6 +45,16 @@ public class Individuo {
     public void setFechaNacimiento(int fechaNacimiento){
         this.fechaNacimiento = fechaNacimiento;
     }
+    public static void setPoblacion(int poblacion){ //Creamos con static 
+        Individuo.poblacion=poblacion; //no ponemos this. ponemos Individuo (el objeto)
+    }
+    public void setAltura(double altura){
+        this.altura = altura;
+    }
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    
     
     public String getDni(){
         return dni;
@@ -39,5 +65,22 @@ public class Individuo {
     public int getFechaNacimiento(){
         return fechaNacimiento;
     }
-
+    public static int getPoblacion(){ //Creamos con static
+        return poblacion;
+    }
+    public double getAltura(){
+        return altura;
+    }
+    public int getEdad(){
+        return edad;
+    }
+    
+    //Metodos que no devuelven ningun valor por eso no son SET o GET
+    public void incrementarAltura() {
+        altura = altura + 0.01; //Incremento 1 cm la altura, está en metros por eso es 0,01
+    }
+    public void saludar(){
+        System.out.println("Hola. Mi nombre es: "+nombre);
+        System.out.println("Encantado de saludarte. Tengo"+edad+" años y mido "+altura+" metros");
+    }
 }//Fin class
